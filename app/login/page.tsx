22eed67fab6ba7a53c2b9669adcdc45cb02c4a85
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-01-11 15:48:33 +0100

    Add Login Page and Protect Access to Dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,31 @@
 const Login = async () => {
     return (
-        <h1>This is the Login Page</h1>
+        <>
+            <main className="framer">
+                <h1>Login to Small Blogger</h1>
+                <p>Create contents for your audience</p>
+                <form>
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="emai"
+                        id="email"
+                        className="input input-border-faint"
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password"
+                        id="password"
+                        className="input input-border-faint"
+                    />
+                    <button 
+                        type="submit"
+                        aria-disabled={false}
+                        aria-description="submit"
+                        className="btn btn-submit btn-primary"
+                    >Login</button>
+                </form>
+            </main>
+        </>
     )
 }
 
---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { cookies } from 'next/headers';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const cookieStore = cookies() 
+    const token = cookieStore.get("user_token") 
+
+    if(token){
+        const response = NextResponse.next({})
+        return response
+    }
+    return NextResponse.redirect(new URL('/', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/dashboard'],
+}
